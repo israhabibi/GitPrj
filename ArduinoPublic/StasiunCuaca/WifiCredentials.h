772c52540743baf117aa5a057a326a605a9926fe
isra <isra.habibi@gmail.com>
2024-01-19 17:06:49 +0700

    add

--- ArduinoPublic/StasiunCuaca/WifiCredentials.h
+++
@@ -1,8 +0,0 @@
-#ifndef WIFICREDENTIALS_H
-#define WIFICREDENTIALS_H
-
-const char* ssid = "ssid24";
-const char* password = "admin12345";
-const char* gas_id = "AKfycby7q3IbJUbLuyWyQ_A2_lsYPozjINJ5D0RV0Rb88UHTEbbt7P5LKnqsbmPfGOwUHCvn0A";
-
-#endif // WIFICREDENTIALS_H
